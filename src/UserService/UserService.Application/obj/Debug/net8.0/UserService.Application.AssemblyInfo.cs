7cc10d58a73f91f6b24e0234076c760460637987
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-10-21 07:20:02 +0700

    update redis server key value

--- src/UserService/UserService.Application/obj/Debug/net8.0/UserService.Application.AssemblyInfo.cs
+++ src/UserService/UserService.Application/obj/Debug/net8.0/UserService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce2a669004f5fb5107a7dc4232d5605bb15f351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f5fea500a2c829afd4a8d27edaeebd62463767")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
